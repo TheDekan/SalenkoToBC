8d4b548697110fc9a009aa17b1ad2a889badb031
TheDekan <ssalenkodd@gmail.com>
2017-10-08 20:23:30 +0300

    Update Command.java

--- src/main/java/com/salenko/command/Command.java
+++ src/main/java/com/salenko/command/Command.java
@@ -11,22 +11,5 @@ public interface Command {
 	void execute();
 
 	boolean validate();
-
-	public static void callHelp() {
-		System.out.println("Available commands: add, edit, remove, all, quit.");
-		System.out.println("To see commands examples - enter them incorrect.");
-	}
-
-	public static void doQuit(boolean[] workOn) {
-		workOn[0] = false;
-		System.out.println("Bye!");
-	}
-
-	public static void readAll(AnnotationConfigApplicationContext context) {
-		List<Book> books = ((BookService) context.getBean("service")).findAll();
-		for (int i = 0; i < books.size(); i++) {
-			System.out.println(i + ". " + books.get(i).getAuthor() + " \"" + books.get(i).getName() + "\"");
-		}
-	}
-
+	
 }
